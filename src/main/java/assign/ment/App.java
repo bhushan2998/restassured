3c5d5b8d5a57e7f024dd530fd2f06d0269b01256
Bhushan Barse <bhushan.barse@ibm.com>
2022-07-04 15:01:48 +0530

    message 1

---
+++ src/main/java/assign/ment/App.java
@@ -0,0 +1,13 @@
+package assign.ment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
